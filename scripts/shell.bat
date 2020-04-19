1c0922c9a0f7a75a5072ff641a7e90e3ca659087
tt <tt3d@start.bg>
2020-04-19 02:21:42 +0200

    Bulding but trowing linking error

--- scripts/shell.bat
+++ scripts/shell.bat
@@ -6,10 +6,9 @@ cd /d %scriptsDir%..
  
 :: uncomment the line below line to debug the vcvars
 :: set VSCMD_DEBUG=1
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
 set path=%scriptsDir%;%path%
  
-:: Add dependencies to our include and lib paths (used by cl.exe)
-set INCLUDE=%scriptsDir%..\deps\SDL\include;%INCLUDE%
-set LIB=%scriptsDir%..\deps\SDL\lib\x64;%LIB%
+REM :: Add dependencies to our include and lib paths (used by cl.exe)
+set INCLUDE=%scriptsDir%..\deps\SDL2-2.0.12\include;%INCLUDE%
+set LIB=%scriptsDir%..\deps\SDL2-2.0.12\lib\x64;%LIB%
